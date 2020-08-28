019b498f25129bada4631d5696914c0687e4ce1a
adaczer <adam.czerwonka@edu.uekat.pl>
2020-08-28 22:14:19 +0200

    finish'

--- www/assets/js/custom.js
+++ www/assets/js/custom.js
@@ -8,6 +8,48 @@ $(function(){
             $('.navbar').removeClass('active');
 
         }
-    })
+
+    });
+
 
 });
+
+
+
+
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+        $('.scrollup').fadeIn();
+    } else {
+        $('.scrollup').fadeOut();
+    }
+});
+
+$('.scrollup').click(function () {
+    $("html, body").animate({
+        scrollTop: 0
+    }, 500);
+    return false;
+});
+
+
+jQuery(function($)
+		{
+			//zresetuj scrolla
+			$.scrollTo(0);
+            
+            $('#link0').click(function() { $.scrollTo($('body'), 600); });
+			$('#link1').click(function() { $.scrollTo($('#about'), 500); });
+			$('#link2').click(function() { $.scrollTo($('#features'), 500); });
+			$('#link3').click(function() { $.scrollTo($('#download'), 500); });
+			$('#link4').click(function() { $.scrollTo($('#pricing'), 500); });
+			$('#link5').click(function() { $.scrollTo($('#contact'), 500); });
+
+		}
+		);
+
+
+
+
+
